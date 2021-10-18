b1cdf7d85ee1fa9a9c1e2d4dd8a6590ed037e105
Eric Jizba <erijiz@microsoft.com>
2021-10-18 16:24:03 -0700

    Init

---
+++ NodeVersion/index.ts
@@ -0,0 +1,10 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    context.res = {
+        body: `Node Version: ${process.version}`
+    };
+};
+
+export default httpTrigger;
