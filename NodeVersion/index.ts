9216a0f575efab7a11aa2d3bedc7827b123058f3
Eric Jizba <erijiz@microsoft.com>
2021-11-09 16:39:19 -0800

    More info in NodeVersion

--- NodeVersion/index.ts
+++ NodeVersion/index.ts
@@ -3,7 +3,9 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 const httpTrigger: AzureFunction = async function (context: Context, _req: HttpRequest): Promise<void> {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.res = {
-        body: `Node Version: ${process.version}`,
+        body: `Version: ${process.version}
+Arch: ${process.arch}
+Modules (abi): ${process.versions.modules}`,
     };
 };
 
