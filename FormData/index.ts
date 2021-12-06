8860963f60b2d9f8c67a67286214649f663d18c6
Eric Jizba <erijiz@microsoft.com>
2021-12-06 15:56:58 -0800

    form updates

--- FormData/index.ts
+++ FormData/index.ts
@@ -7,6 +7,14 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
         : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
 
+    const form = await req.parseFormBody();
+    console.log('(get) name = ' + form.get('name')?.value);
+    console.log('(get) greeting = ' + form.get('greeting')?.value);
+
+    for (const [name, part] of form) {
+        console.log(`(for-loop) ${name} = ${part.value.toString()}`);
+    }
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage,
