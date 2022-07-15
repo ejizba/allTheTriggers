c00f6b1c7dcc45b98917197d1bd7338eba0f4c33
Eric Jizba <erijiz@microsoft.com>
2022-07-15 13:15:49 -0700

    fix form data

--- FormData/index.ts
+++ FormData/index.ts
@@ -2,22 +2,17 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = req.query.name || (req.body && req.body.name);
-    const responseMessage = name
-        ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-        : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
-
-    /*const form = await req.parseFormBody();
+    const form = req.parseFormBody();
     console.log('(get) name = ' + form.get('name')?.value);
     console.log('(get) greeting = ' + form.get('greeting')?.value);
 
     for (const [name, part] of form) {
         console.log(`(for-loop) ${name} = ${part.value.toString()}`);
-    }*/
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage,
+        body: 'form stuff',
     };
 };
 
