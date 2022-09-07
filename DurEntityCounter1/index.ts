224af95bd75a60fc53d6075afe66b086d3f6e08c
Eric Jizba <erijiz@microsoft.com>
2022-09-07 14:56:10 -0700

    Add durable entity

---
+++ DurEntityCounter1/index.ts
@@ -0,0 +1,36 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ *
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *    function app in Kudu
+ */
+
+import * as df from 'durable-functions';
+
+const myFunc = df.entity(function (context) {
+    let currentValue: number = <number>context.df.getState(() => 0);
+    context.log('Entity state: ', currentValue);
+
+    switch (context.df.operationName) {
+        case 'add': {
+            const amount = <number>context.df.getInput();
+            currentValue += amount;
+            break;
+        }
+        case 'reset':
+            currentValue = 0;
+            break;
+        case 'get':
+            context.df.return(currentValue);
+            break;
+    }
+
+    context.df.setState(currentValue);
+    context.log('Entity state: ', currentValue);
+});
+
+export default myFunc;
---
+++ DurOrchestratorCounter1/index.ts
@@ -0,0 +1,12 @@
+﻿import * as df from 'durable-functions';
+
+const orchestrator = df.orchestrator(function* (context) {
+    const entityId = new df.EntityId('DurEntityCounter1', 'myAsyncCounter');
+
+    const currentValue = yield context.df.callEntity(entityId, 'get');
+    if (currentValue < 10) {
+        yield context.df.callEntity(entityId, 'add', 1);
+    }
+});
+
+export default orchestrator;
